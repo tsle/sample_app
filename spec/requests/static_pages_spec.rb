85c941715558e8c561ce4b3431849e0786e67555
tsle <les.tuan@gmail.com>
2013-09-13 19:23:44 -0400

    Finish User model and authentication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,38 +2,54 @@ require 'spec_helper'
 
 
 describe "Static pages" do
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    links = ["About", "Help", "Contact", "Home", "Sign up now!", "sample app"]
+    titles = ["About", "Help", "Contact", "", "Sign up", ""]
+    links.each_with_index do |val, index|
+      click_link val
+      expect(page).to have_title(full_title(titles[index]))
+    end
+  end
 
   describe "Home page" do
     before { visit root_path }
-    subject { page }
-    it { should have_content('Sample App') }
-    it { should have_title(full_title '') }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    subject { page }
-    
-    it { should have_content('Help') }
-    it { should have_title(full_title "Help") }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }    
 
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    subject { page }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title "About Us") }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    subject { page }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title "Contact") }
+    it_should_behave_like "all static pages"
   end
 
 end
