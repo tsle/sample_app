abf1e21eaf9c0866322320ffe47fe195b67d52c4
tsle <les.tuan@gmail.com>
2013-09-15 16:09:04 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,22 +25,25 @@ describe "UserPages" do
     describe "with invalid information" do
       let(:user) { FactoryGirl.create(:user) }
       it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        expect{ click_button submit }.not_to change(User, :count)
       end
 
       it "should receive email invalid error message" do
         user.email = "notvalid@"
-        expect { click_button submit }.not_to have_content("Email is invalid")
+        click_button submit
+        should have_content("Email is invalid")
       end
 
       it "should receive password to short error" do
         user.password = user.password_confirmation = "short"
-        expect { click_button submit }.not_to have_content("Email is invalid")
+        click_button submit
+        should have_content("Email is invalid")
       end
 
       it "should receive invalid name error" do
         user.name = " "
-        expect { click_button submit }.not_to have_content("Email is invalid")
+	click_button submit
+	should have_content("Email is invalid")
       end     
       
     end
@@ -57,6 +60,15 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+	let(:user) { User.find_by(email: "jerry@yahoo.com") }
+
+	it { should have_title(user.name) }
+	it { should have_link("Sign out") }
+	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end              
     end
   end
 end
